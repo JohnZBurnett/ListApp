6edfe0f7cc9d8913217701900df9bff4e31604d8
JohnZBurnett <tuninator@gmail.com>
2018-10-27 16:29:47 -0400

    write ListContainer skeleton

---
+++ src/ListContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ListsContainer extends Component {
+    constructor() {
+        super(); 
+        this.state = {
+            lists: []
+        }
+    }
+
+    render() {
+        return(
+            <div>This is the ListContainer.</div>
+        )
+    }
+}
+
+export default ListsContainer; 
